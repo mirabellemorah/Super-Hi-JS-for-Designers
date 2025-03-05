f1065f4e427813ef111691cb0836151e98977773
Mirabelle Morah <myrakuti@gmail.com>
2025-03-05 11:34:53 +0000

    update

--- rose-culver/script.js
+++ rose-culver/script.js
@@ -8,10 +8,20 @@ const circleTag = document.querySelector("section div.circle");
 const bodyTag = document.querySelector("body");
 
 const pages = [
-    { copy: "a dear friend", background: "#edc7a9", circle: "#3e78ed" },    // 0
-    { copy: "the love of my life", background: "#a1c45a", circle: "#f9a03f" },    // 1        
-    { copy: "my constant", background: "#f9a03f", circle: "#ed3e3e" },    // 2
-    { copy: `my partner <a href="https://www.mirabellemorah.com">in crime</a>`, background: "#f9845b", circle: "#19d8e9" },    // 3
+    { copy: "The best opportunities don’t often come from joining the crowd but serving it — JM Marco", background: "#edc7a9", circle: "#3e78ed" },    // 0
+    { copy: "Do not forget to meet all your difficulties with love and laughter - God calling Devotional", background: "#a1c45a", circle: "#f9a03f" },    // 1        
+    { copy: "Life isn’t as serious as my mind makes it out to be. - Ekhart Tolle", background: "#f9a03f", circle: "#ed3e3e" },    // 2
+    { copy: `A good partner should reduce the complexity and difficulty of your life, not add to it — EY on Fishbowl`, background: "#f9845b", circle: "#19d8e9" },    // 3
+    { copy: `Your focus determines your reality`, background: "#A9245c", circle: "#39d8e9" },    // 4
+    { copy: `The unconscious compulsion to enhance one’s identity through association with an object is built into the very structure of the egoic mind — Eckhart Tolle`, background: "#f8345b", circle: "#00be96" },    // 5
+    { copy: `Paradoxically, what keeps the so called consumer society going is the fact that trying to find yourself through things doesn’t work: the ego satisfaction is short lived and so you keep looking for more, keep buying, keep consuming. — Eckhart Tolle`, background: "#f8ba5b", circle: "#063aad" },    // 6
+    { copy: `Once this life is over there are no redos`, background: "#960af2", circle: "#0034ff" },    // 7
+    { copy: `Whatever you appreciate appreciates. Give thanks and practice gratitude - Nat Bassey`, background: "#ab237d", circle: "#77cf96" },    // 8
+    { copy: `Great value precedes great wealth. If you want to make millions impact millions. `, background: "#f9f8f0", circle: "#Ab234f" },    // 9
+    { copy: `The value of a man should be seen in what he can give and not what he receives, Albert Einstein`, background: "#fab6a8", circle: "#f7a8d0" },    // 10
+    { copy: `Fortune favours the prepared`, background: "#c3eed1", circle: "#f9f896" },    // 11
+    { copy: `For it is not in success where true character is revealed but in failure. - Black Manta`, background: "#ff1f56", circle: "#00ce4c" },    // 12
+    { copy: `The world is full of magical things patiently waiting for our senses to grow sharper. - W.B. Yeats`, background: "ff9ca5", circle: "#000" },    // 13
 ]
 
 const next = function () {
@@ -56,3 +66,15 @@ randomTag.addEventListener("click", () => {
 prevTag.addEventListener("click", () => {
     prev();
 });
+
+document.addEventListener("keyup", (event) => {
+
+    if (event.key === "ArrowRight") {
+        next();
+    }
+
+    if (event.key === "ArrowLeft") {
+        prev();
+    }
+
+})
