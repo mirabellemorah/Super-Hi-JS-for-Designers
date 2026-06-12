48753370d802ccab7f65eb5e3a416da9f82d8b2c
mirabellemorah <myrakuti@gmail.com>
2026-06-12 13:42:50 +0100

    Picking up in 2026 to finish what I started

---
+++ brux/eyes.js
@@ -0,0 +1,13 @@
+const irisLeft = document.querySelector(".iris-left");
+const irisRight = document.querySelector(".iris-right");
+const face = document.querySelector(".face");
+
+const moveEyes = function (tag, mouseX, mouseY) {
+
+
+};
+
+document.addEventListener("mousemove", function (event) {
+
+
+});
