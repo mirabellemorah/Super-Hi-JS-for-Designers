dff7100f8c76781646d2beed205591d1d282b9e9
mirabellemorah <myrakuti@gmail.com>
2026-06-12 15:42:42 +0100

    reworked js

--- brux/header.js
+++ brux/header.js
@@ -1,4 +1,4 @@
-const headerTag = document.querySelector("header nav");
+/*const headerTag = document.querySelector("header nav");
 
 const toggleHeader = function () {
   const pixelsFromTop = window.pageYOffset;
@@ -16,5 +16,35 @@ const toggleHeader = function () {
 toggleHeader();
 
 document.addEventListener("scroll", (e) => {
+  toggleHeader();
+}); */
+
+//when i scroll down the page
+// change the background color of the header and add a box shadow
+
+const headerTag = document.querySelector("header nav");
+const scrollIndicator = document.querySelector(".scroll-indicator");
+const colors = ["#f8c0d5ff", "#79eae8", "#f9e79f", "#a9cce3", "#d7bde2"];
+
+function toggleHeader() {
+  const pixels = window.pageYOffset;
+  const colorIndex = Math.floor(pixels / 200) % colors.length;
+  scrollIndicator.innerHTML = pixels;
+
+  if (pixels <= 100) {
+    headerTag.style.backgroundColor = "";
+    scrollIndicator.style.backgroundColor = "";
+    headerTag.style.boxShadow = "none";
+    return;
+  }
+
+  headerTag.style.backgroundColor = colors[colorIndex];
+  scrollIndicator.style.backgroundColor = colors[colorIndex];
+  headerTag.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)";
+  scrollIndicator.style.transition = "all 0.9s ease-in-out";
+}
+
+toggleHeader();
+document.addEventListener("scroll", function () {
   toggleHeader();
 });
